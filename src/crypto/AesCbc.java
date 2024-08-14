c8d9799c8d52db04134534909010e9b76b0c56d6
大塚　泰司 <yasushi-ohtsuka@nifty.ne.jp>
2024-08-15 00:00:19 +0900

    追加

--- src/crypto/AesCbc.java
+++ src/crypto/AesCbc.java
@@ -9,25 +9,21 @@ import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
 
-public class AesGcm {
+public class AesCbc {
 
 	/** AES */
 	public static final String AES = "AES";
-	/** AES/GCM/NoPadding */
-	public static final String AES_GCM_NO_PADDING = "AES/GCM/NoPadding";
-	/** GCMのIVの長さ */
-	public static final int GCM_IV_LENGTH = 12;
+	/** AES/CBC/PKCS5Padding */
+	public static final String AES_CBC_PKCS5PADDING = "AES/CBC/PKCS5Padding";
+	/** IVの長さ */
+	public static final int GCM_IV_LENGTH = 16;
 	/** AESのキーの長さ ( 128 or 192 or 256 ) */
 	public static final int AES_KEY_SIZE = 256;
-	/** GCMのタグの長さ ( 96 or 104 or 112 or 120 or 128 ) */
-	private static final int GCM_TAG_LENGTH = 128;
 
 	// 平文
-	private static String plainText = "こんにちは。この文を「AES-GCM」の暗号化アルゴリズムによって暗号化します。";
-	// ADD (64KiB以内)
-	private static String addText = "This is an additional authenticated data. It's called the AAD. You can use any value.";
+	private static String plainText = "こんにちは。この文を「AES-CBC」の暗号化アルゴリズムによって暗号化します。";
 
 	public static void main(String[] args) throws GeneralSecurityException {
 
@@ -41,24 +37,21 @@ public class AesGcm {
 		SecureRandom secureRandom = new SecureRandom();
 		secureRandom.nextBytes(iv);
 
-		// GCMParameterSpecインスタンス生成
-		GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH, iv);
-
-		// add（追加認証）生成
-		byte[] aad = addText.getBytes(StandardCharsets.UTF_8);
+		// IvParameterSpecインスタンス生成
+		IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
 
 		// 平文出力
 		System.out.println("Original Text : " + plainText);
 		byte[] plainTextData = plainText.getBytes(StandardCharsets.UTF_8);
 
 		// 暗号化
-		byte[] encryptedData = encrypt(plainTextData, secretKey, gcmParameterSpec, aad);
+		byte[] encryptedData = encrypt(plainTextData, secretKey, ivParameterSpec);
 
 		// 暗号化データ出力
 		System.out.println("Encrypted Text : " + Base64.getEncoder().encodeToString(encryptedData));
 
 		// 複合化
-		byte[] decryptedData = decrypt(encryptedData, secretKey, gcmParameterSpec, aad);
+		byte[] decryptedData = decrypt(encryptedData, secretKey, ivParameterSpec);
 
 		// 複合化データ出力
 		System.out.println("Decrypted Text : " + new String(decryptedData, StandardCharsets.UTF_8));
@@ -70,23 +63,17 @@ public class AesGcm {
 	 * @param plainData 平文データ
 	 * @param key       暗号化キー
 	 * @param params    暗号パラメータ
-	 * @param aad       追加認証
 	 * @return 暗号化データ
 	 * @throws GeneralSecurityException
 	 */
-	public static byte[] encrypt(byte[] plainData, SecretKey secretKey, AlgorithmParameterSpec params, byte[] aad)
+	public static byte[] encrypt(byte[] plainData, SecretKey secretKey, AlgorithmParameterSpec params)
 			throws GeneralSecurityException {
 
 		// Cipherインスタンス生成
-		Cipher cipher = Cipher.getInstance(AES_GCM_NO_PADDING);
+		Cipher cipher = Cipher.getInstance(AES_CBC_PKCS5PADDING);
 
 		// Cipherに暗号モードを設定
 		cipher.init(Cipher.ENCRYPT_MODE, secretKey, params);
-
-		// ADD（追加認証）設定
-		if (aad != null) {
-			cipher.updateAAD(aad);
-		}
 
 		// 暗号化
 		byte[] cipherData = cipher.doFinal(plainData);
@@ -100,22 +87,16 @@ public class AesGcm {
 	 * @param cipherData 暗号化データ
 	 * @param key        暗号キー
 	 * @param params     暗号パラメータ
-	 * @param aad        追加認証
 	 * @return 複合化データ
 	 * @throws GeneralSecurityException
 	 */
-	public static byte[] decrypt(byte[] cipherData, SecretKey key, AlgorithmParameterSpec params, byte[] aad)
+	public static byte[] decrypt(byte[] cipherData, SecretKey key, AlgorithmParameterSpec params)
 			throws GeneralSecurityException {
 		// Cipherインスタンス生成
-		Cipher cipher = Cipher.getInstance(AES_GCM_NO_PADDING);
+		Cipher cipher = Cipher.getInstance(AES_CBC_PKCS5PADDING);
 
 		// Cipherに複合モードを設定
 		cipher.init(Cipher.DECRYPT_MODE, key, params);
-
-		// ADD（追加認証）設定
-		if (aad != null) {
-			cipher.updateAAD(aad);
-		}
 
 		// 複合化
 		byte[] decryptedData = cipher.doFinal(cipherData);
